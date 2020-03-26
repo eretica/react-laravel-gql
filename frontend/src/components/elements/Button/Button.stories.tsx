bc0ca5a68c1bfbfe064d6be3f7b792ee0e4e94d8
eretica <eretica.m@gmail.com>
2020-03-26 11:21:22 +0900

    Update Button.stories.tsx

--- frontend/src/components/elements/Button/Button.stories.tsx
+++ frontend/src/components/elements/Button/Button.stories.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import {storiesOf} from "@storybook/react";
 import {Button} from './Button'
 
+// add comment
 storiesOf('Elements/Button', module)
   .add('all', () => <Button>Click Me!</Button>)
